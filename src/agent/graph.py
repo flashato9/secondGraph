e47482a69eaf00971b153cc5cc8672fc3256b4f7
flashato9 <ato_koomson@hotmail.ca>
2026-03-29 17:54:34 -0400

    mod 3 - implemented Human In The Loop in LangSmith Created asssistants in langsmith Understood forking in langsmith Implemented HITL created a dataset in langsmith Note: HITL has conflict with experiments because experiments cannot handle hitl loops
    I also enabled tracing in the langsmith

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -4,27 +4,34 @@ Returns a predefined response. Replace logic and configuration as needed.
 """
 
 from __future__ import annotations
+from dataclasses import dataclass
 from math import remainder
+from os import system
 from re import S
 from typing import Annotated, Literal
 
 from dotenv import load_dotenv
-from langgraph.graph import END, START, StateGraph
+from langgraph.graph import END, START, StateGraph, add_messages
 from langgraph.prebuilt import ToolNode, tools_condition
-from langchain_core.messages import AIMessage, AnyMessage, HumanMessage
+from langchain_core.messages import AIMessage, AnyMessage, HumanMessage, RemoveMessage, SystemMessage
+from langgraph.types import Command, interrupt
+from langchain_core.messages import convert_to_messages
+from pydantic import BaseModel, Field
 from typing_extensions import TypedDict
 import base64
 import aiofiles
 import json
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langgraph.runtime import Runtime
+from pathlib import Path
 
+load_dotenv()
 
-class State(TypedDict):
-    messages: Annotated[list[AnyMessage], add_reducer]
 
 def add_reducer(existing_message:list[AnyMessage], new_message: list[AnyMessage]) -> list[AnyMessage]:
     # note to self, the reducer applies to a member of the state. It takes two of the same type of memeber and generates one.
-    result = (existing_message if existing_message else []) + (new_message if new_message else [])
+    add_messages_result = add_messages(existing_message,new_message)
+    result = [elem for elem in add_messages_result]
     return result
 
 async def getPicture(path: str) -> list:
@@ -42,8 +49,9 @@ async def getPicture(path: str) -> list:
     image_bytes = None
     async with aiofiles.open(path, mode='rb') as f:
         image_bytes = await f.read()
+    file_extension = Path(path).suffix.replace(".","")
     image64 = base64.b64encode(image_bytes).decode("utf-8")
-    mime_type = "image/png"
+    mime_type = f"image/{file_extension}"
     content =[
         # {
         #     "type": "text",
@@ -57,23 +65,27 @@ async def getPicture(path: str) -> list:
         {
             "type": "image_url",
             "image_url": {
-                    "url": f"data:image/jpeg;base64,{image64}"
+                    "url": f"data:{mime_type};base64,{image64}"
                 },
         },
         
     ]
     return content
 
-load_dotenv()
-tools = [getPicture]
-model = ChatGoogleGenerativeAI(
-    model="gemini-2.5-flash",
-    temperature=1.0,
-    max_tokens=None,
-    timeout=None,
-    max_retries=2,
-)
-llm_with_tools = model.bind_tools(tools)
+
+
+async def get_llm(llm_config: LLMConfiguration):
+    model = ChatGoogleGenerativeAI(
+        model=llm_config.model_name,
+        temperature=llm_config.temperature,
+        max_tokens=None,
+        timeout=None,
+        max_retries=2,
+        )
+    tools = [getPicture]
+    llm_with_tools = model.bind_tools(tools)
+    return llm_with_tools
+
 
 def compress_text_content(message:AIMessage):
     #note to self: the content here is also a list of content blocks. A multi-modal model accepts as input a list of content blocks and returns as output a list of content blocks.
@@ -95,23 +107,104 @@ def compress_text_content(message:AIMessage):
         result_content = all_text_content + non_text_content
     message.content = result_content
     return message
+    
+class State(TypedDict):
+    messages: Annotated[list[AnyMessage], add_reducer]
+    
+class LLMConfiguration(BaseModel):
+    "the configuration for the llm"
+    model_name: str = "gemini-2.5-flash"
+    temperature: float = 1.0
 
-async def roomEvaluator(state: State) -> State:
+class ContextSchema(BaseModel):
+    #note to self: only pydantic basemodel allows default values. TypedDict does not generate default values.
+    #note to self: you can use the json_schema extra and use doc string and description to better explain the configuration and it can also be recursive.
+    #note to self: its typically best to use simple types where best and complicate things if they work. But stnadard types like string, int, float. Those work well
+    #note to self: you can create node scoped assistant configurations.
+    llm_configuration: LLMConfiguration = LLMConfiguration()
+    persona: str = Field(
+        default = (
+            "Your name is RoomCleanlinessEvaluator\n" +
+            "You are a specialist at knowing the cleanliness of a room.\n"+
+            "Your task is to provide the user information about the cleaniless of their room.\n"+
+            "If the user's prompt is not related to the cleanliness of a room, then respond with 'I cannot answer that question, I am only an expert at knowing the cleanliness of rooms'\n"
+        ),
+        description = "The persona for the assistant",
+        json_schema_extra={
+            "langgraph_nodes": ["room_evaluator"],
+            "langgraph_type": "prompt"
+        }
+    )
+
+async def room_evaluator(state: State, runtime: Runtime[ContextSchema]) -> State:
     #note to self: a node takes in a state and outputs a state.
+    #note to self: a Pydantic BaseModel requires you to access to object via dot operator and not using square brackets as you would do with TypedDict  
     model_input = state["messages"]
-    ai_message = await llm_with_tools.ainvoke(model_input)
+    llm_persona = SystemMessage(content= runtime.context.persona)
+    llm_config = runtime.context.llm_configuration
+    llm_with_tools = await get_llm(llm_config)
+    ai_message = await llm_with_tools.ainvoke([llm_persona] + model_input)
     ai_message = compress_text_content(ai_message)
     result = State(
         messages = [ai_message]
     )
     return result
+
+async def toolApprover(state:State, runtime:Runtime[ContextSchema]) -> Literal["tools","prompt_editor", "prompt_rejecter","__end__"]:
+    is_last_message_contains_tool_call = len(state["messages"][-1].tool_calls)>0
+    if is_last_message_contains_tool_call:
+        interrupt_return_message = interrupt({
+            "query": "Do you approve the tool call?",
+            "answer_options": "Approve/Edit/Reject",
+            "tool_call":state["messages"][-1].tool_calls
+        })
+        if interrupt_return_message == "Approve":
+            return "tools"
+        if interrupt_return_message == "Edit":
+            return "prompt_editor"
+        if interrupt_return_message == "Reject":
+            return "prompt_rejecter"
+    else:
+        return END
+
+async def prompt_editor(state: State, runtime: Runtime[ContextSchema]) -> State:
+    messages = state["messages"]
+
+    previous_message = messages[-2].content
+    new_message = interrupt({
+    "query": f"""
+        The previous prompt was - {previous_message}.
+        Enter the prompt to replace this prompt.
+    """
+    })
+    messages_to_remove = [RemoveMessage(id=m.id) for m in messages[-2:]]
+    message_to_add = HumanMessage(
+        content = new_message
+    )
+    return State(
+        messages = messages_to_remove + [message_to_add]
+    )
+
+async def prompt_rejecter(state: State, runtime: Runtime[ContextSchema]) -> State:
+    messages = state["messages"]
+    messages_to_remove = [RemoveMessage(id=m.id) for m in messages[-2:]]
+    return State(
+        messages = messages_to_remove
+    )
+
 graphName = "Room Evaluator Agent"
 graph = ( 
-    StateGraph(State)
-    .add_node("roomEvaluator",roomEvaluator)
+    StateGraph(State, context_schema =ContextSchema)
+    .add_node("room_evaluator",room_evaluator)
+    .add_node("prompt_editor",prompt_editor)
+    .add_node("prompt_rejecter",prompt_rejecter)
     .add_node("tools",ToolNode([getPicture]))
-    .add_edge(START, "roomEvaluator")
-    .add_edge("tools","roomEvaluator")
-    .add_conditional_edges("roomEvaluator",tools_condition)
-    .compile(name=graphName)
+    .add_edge(START, "room_evaluator")
+    .add_edge("tools","room_evaluator")
+    .add_conditional_edges("room_evaluator",toolApprover)
+    .add_edge("prompt_editor","room_evaluator")
+    .add_edge("prompt_rejecter",END)
+    .compile(
+        name=graphName
+        )
 )
